4da9c6a432c3de1ffbd5b50cd24f5398e406da79
Alexander Gates <gatesa@18sh704md19.ocas.uc.edu>
2025-06-17 15:32:55 -0400

    button overhaul 60/100% done

--- packages/client/src/pages/Home.tsx
+++ packages/client/src/pages/Home.tsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import './home.css';
 import { Student } from './StudentSelect';
 import { PastEval } from './PastEvaluations';
+import '../components/buttonandcard.css';
 
 const fetchUrl = `http://localhost:${3001}`;
 
--- packages/client/src/pages/PastEvaluations.tsx
+++ packages/client/src/pages/PastEvaluations.tsx
@@ -4,6 +4,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './PastEvaluations.css';
 import { useLocation } from 'react-router';
+import '../components/buttonandcard.css';
 
 type Selections = Record<string, PerformanceLevel>;
 
