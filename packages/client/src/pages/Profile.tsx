e92b3d086a8e68f6acf0abee9e1d675cb22c776c
Kyle Barker <barkerk@18sh704md21.ocas.uc.edu>
2025-06-26 14:00:50 -0400

    eslint fixes to satify the beast

--- packages/client/src/pages/Profile.tsx
+++ packages/client/src/pages/Profile.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../CSS/Profile.css';
-import '../components/buttonandcard.css';
+import '../components/buttonAndCard.css';
 import '../components/Modals.css';
 
 const fetchUrl = `http://localhost:3001`;
--- packages/client/src/pages/Supervisor.tsx
+++ packages/client/src/pages/Supervisor.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../CSS/Supervisor.css';
-import '../components/buttonandcard.css';
+import '../components/buttonAndCard.css';
 import '../components/Modals.css';
 const fetchUrl = `http://localhost:${3001}`;
 interface Student {
