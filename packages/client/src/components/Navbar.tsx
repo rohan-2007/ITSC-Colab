a0dce56d1c33346fb2f55472a98c32dbaf31be55
Kyle Barker <barkerk@18sh704md21.ocas.uc.edu>
2025-06-30 11:48:36 -0400

    c

--- packages/client/src/components/Navbar.tsx
+++ packages/client/src/components/Navbar.tsx
@@ -2,34 +2,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import { useNavigate } from 'react-router-dom';
 import logo from '../assets/ITSC_LOGO.png';
 
-const Navbar: React.FC = () => {
-  const navigate = useNavigate();
-
-  return <header className="navbar">
-    <img src={logo} className="navbar-logo" />
-    <nav className="nav-links">
-      <Link to="/">Home</Link>
-      <Link to="/evaluations">Evaluations</Link>
-      <Link to="/profile">Profile</Link>
-      {(localStorage.getItem(`userMeta`) &&
+const Navbar: React.FC = () => <header className="navbar">
+  <img src={logo} className="navbar-logo" />
+  <nav className="nav-links">
+    <Link to="/">Home</Link>
+    <Link to="/evaluations">Evaluations</Link>
+    <Link to="/profile">Profile</Link>
+    {(localStorage.getItem(`userMeta`) &&
       JSON.parse(localStorage.getItem(`userMeta`) as string).role === `SUPERVISOR`) &&
         <Link to="/supervisor">Supervisor</Link>}
-      <Link to="/contact">Contact</Link>
-    </nav>
-    <button
-      className="logout-btn"
-      onClick={async () => {
-        await fetch(`http://localhost:3001/logout/`, {
-          credentials: `include`,
-          method: `POST`,
-        });
-        localStorage.clear();
-        window.location.reload();
-      }}
-    >Log Out</button>
-  </header>;
-};
+    <Link to="/contact">Contact</Link>
+  </nav>
+  <button
+    className="logout-btn"
+    onClick={async () => {
+      await fetch(`http://localhost:3001/logout/`, {
+        credentials: `include`,
+        method: `POST`,
+      });
+      localStorage.clear();
+      window.location.reload();
+    }}
+  >Log Out</button>
+</header>;
 export default Navbar;
