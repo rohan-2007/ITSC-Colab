4cadede93339a2aaad603352752b90f5ca3cb774
Rohan Singh <singhr@18sh704md10.ocas.uc.edu>
2025-07-03 17:07:43 -0400

    added add level and delete level

--- packages/server/src/routes/rubric.ts
+++ packages/server/src/routes/rubric.ts
@@ -5,8 +5,10 @@ import { limiter } from './auth';
 const router = Router();
 
 interface RubricRequestBody {
+  addLevel?: boolean;
   categoryId?: number;
   categoryTitle?: string;
+  deletedLevel?: number;
   description?: string;
   level?: string;
   levelId?: number;
@@ -25,6 +27,9 @@ router.get(`/rubric`, limiter, async (req: Request, res: Response): Promise<void
             description: true,
             level: true,
           },
+          where: {
+            deletedAt: null,
+          },
         },
         subItems: {
           select: {
@@ -36,6 +41,9 @@ router.get(`/rubric`, limiter, async (req: Request, res: Response): Promise<void
       orderBy: {
         displayOrder: `asc`,
       },
+      where: {
+        deletedAt: null,
+      },
     });
     res.status(200).json(rubricCategories);
   } catch {
@@ -44,7 +52,10 @@ router.get(`/rubric`, limiter, async (req: Request, res: Response): Promise<void
 });
 
 router.post(`/changeRubric`, limiter, async (req: Request<unknown, unknown, RubricRequestBody>, res: Response) => {
-  const { categoryId, categoryTitle, description, level, levelId, prevLevel, subItem, subItemId } = req.body;
+  const {
+    addLevel, categoryId, categoryTitle, deletedLevel, description,
+    level, levelId, prevLevel, subItem, subItemId,
+  } = req.body;
 
   if (description && categoryId && levelId) {
     try {
@@ -96,6 +107,39 @@ router.post(`/changeRubric`, limiter, async (req: Request<unknown, unknown, Rubr
       return;
     } catch {
       res.status(500).json({ error: `Internal Server Error` });
+      return;
+    }
+  } else if (addLevel) {
+    const categories = await prisma.rubricCategory.findMany();
+    const levelData = await prisma.rubricPerformanceLevel.findMany();
+    const levels = levelData.filter((l) => l.rubricCategoryId === 1).length;
+
+    for (const category of categories) {
+      await prisma.rubricPerformanceLevel.create({
+        data: {
+          id: category.id * 100 + levels + 1,
+          description: ``,
+          level: ``,
+          rubricCategoryId: category.id,
+        },
+      });
+    }
+  } else if (deletedLevel) {
+    // console.log(`deleted level: `, deletedLevel);
+    try {
+      const levelNumber = deletedLevel % 10;
+      const time = new Date();
+
+      await prisma.$executeRaw`
+        UPDATE "rubricPerformanceLevel"
+        SET "deletedAt" = ${ time }
+        WHERE "id" % 10 = ${ levelNumber };
+      `;
+
+      res.status(200).json({ message: `deleted level successfully` });
+      return;
+    } catch (error) {
+      res.status(500).json({ error, message: `Internal Server Error` });
       return;
     }
   }
